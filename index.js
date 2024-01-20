3f76f44b81be74c66f9a3415fd6dd318aeb8635a
HovhannesK <hovhanneskeshishyan@gmail.com>
2024-01-20 22:03:57 +0400

    try fix vercel routes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const app = express();
+const index = express();
 const cors = require('cors');
 require("dotenv").config();
 
 const router = require("./src/routes/index");
 const corsConfig = require("./config/cors-config");
 
-app.use(cors(corsConfig));
-app.use(router);
+index.use(cors(corsConfig));
+index.use(router);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+index.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
 })
 
-module.exports = app;
+module.exports = index;
